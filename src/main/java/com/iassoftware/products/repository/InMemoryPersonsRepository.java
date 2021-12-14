f64d30e8130dc354531e22ae437ccb4518291845
David McEwen Arango <dmcewena@hotmail.com>
2021-12-14 07:05:51 -0500

    adding more comments

--- src/main/java/com/iassoftware/products/repository/InMemoryPersonsRepository.java
+++ src/main/java/com/iassoftware/products/repository/InMemoryPersonsRepository.java
@@ -2,7 +2,6 @@ package com.iassoftware.products.repository;
 
 import com.iassoftware.products.domain.Product;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.*;
@@ -12,25 +11,27 @@ public class InMemoryPersonsRepository {
 //The base class InMemoryJpaRepository makes it easy to create in-memory versions of Spring Data JPA repositories.
 // The reason to use such repositories when testing is that they can be created and destroyed very quickly.
     @Component
-    public class InMemoryProductsRepository implements ProductRepository {
+    public class InMemoryProductsRepository implements ProductRepository { //Implement all methods defined for ProductRepository
         private final Map<String, Product> database = new HashMap<>(); //Is like Javascript object or Python Dictionary
 
         @Override
         public List<Product> list() {
-            Collection<Product> values = database.values();
+            Collection<Product> values = database.values(); //Key:VALUE
             return new ArrayList<>(values);
         }
 
         @Override
         public Product findOne(String referenceId) {
+
             return database.get(referenceId);
         }
 
         @Override
         public void create(Product product) {
             Product foundProduct = database.get(product.getReferenceId());
+            //If ID already exist, we don't create a new product
             if (foundProduct != null) {
-                throw new IllegalArgumentException("Person with id: " + product.getReferenceId() + " already exists.");
+                throw new IllegalArgumentException("Product with id: " + product.getReferenceId() + " already exists.");
             }
             database.put(product.getReferenceId(), product);
         }
@@ -38,8 +39,9 @@ public class InMemoryPersonsRepository {
         @Override
         public void update(String ReferenceId, Product product) {
             Product foundProduct = database.get(ReferenceId);
+            //If ReferenceId return null... throw an exception
             if (foundProduct == null) {
-                throw new IllegalArgumentException("Person with id: " + ReferenceId  + " not found");
+                throw new IllegalArgumentException("Product with id: " + ReferenceId  + " not found");
             }
 
             database.put(ReferenceId, product);
@@ -49,12 +51,13 @@ public class InMemoryPersonsRepository {
         public void delete(String ReferenceId) {
             Product foundProduct = database.get(ReferenceId);
             if (foundProduct == null) {
-                throw new IllegalArgumentException("Person with id: " + ReferenceId + " not found");
+                throw new IllegalArgumentException("Product with id: " + ReferenceId + " not found");
             }
 
             database.remove(ReferenceId);
 
         }
+
     }
 
 }
